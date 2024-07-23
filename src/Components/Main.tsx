751ae8580153cd259fd468aeca303c8f6ee1f3e4
Wambui Ndungu <wambuin18@gmail.com>
2024-07-23 15:19:17 +0300

    update horizontal scrolling

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -7,21 +7,23 @@ interface MainProps {
   children: ReactNode;
 }
 
-const Main: React.FC<MainProps> = ({ children }) => {
-  const targetRef = useRef(null);
-  const { scrollYProgress } = useScroll({
-    target: targetRef,
-  });
-  const x = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+const Main = ({ children }: MainProps) => {
+  const targetRef = useRef<HTMLDivElement | null>(null);
+
+  const { scrollYProgress } = useScroll({ target: targetRef });
+  const x = useTransform(scrollYProgress, [0, 1], ["0%", "-100%"]);
 
   return (
-    <motion.main
-      style={{ x }}
-      ref={targetRef}
-      className="flex overflow-x-auto w-screen h-screen"
-    >
-      {children}
-    </motion.main>
+    <main className="relative h-auto md:h-[500vh]" ref={targetRef}>
+      <div className="sticky top-0 overflow-hidden md:h-screen py-[120px] responsive">
+        <motion.div
+          style={{ x }}
+          className="md:h-full flex flex-col md:flex-row gap-4 w-full md:w-[100%]"
+        >
+          {children}
+        </motion.div>
+      </div>
+    </main>
   );
 };
 
--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -14,9 +14,19 @@ export default function About() {
           ecosystems in Kenya as well as development of neighbouring communities
         </p>
       </div>
-      <div className="md:w-[50vw] w-full md:h-full h-auto bg-orange"></div>
-      <div className="md:w-[50vw] w-full md:h-full h-auto bg-orange"></div>
-      <div className="md:w-[50vw] w-full md:h-full h-auto bg-orange"></div>
+      <div className="md:w-[50vw] w-full md:h-full h-[50vh] bg-orange"></div>
+      <div className="md:w-[50vw] w-full md:h-full h-auto py-6 flex flex-col gap-6 justify-center">
+        <h3 className={`uppercase text-4xl font-semibold ${crimson.className}`}>
+          About
+        </h3>
+        <p className="">
+          African Plains Conservation of Trust is a non-profit organization that
+          works towards sustainable conservation of select habitats and
+          ecosystems in Kenya as well as development of neighbouring communities
+        </p>
+      </div>
+      <div className="md:w-[50vw] w-full md:h-full h-[50vh] bg-orange"></div>
+      <div className="md:w-[50vw] w-full md:h-full h-[50vh] bg-orange"></div>
     </Main>
   );
 }
