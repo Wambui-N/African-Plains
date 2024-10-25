f282f205cd723a5eec26703758c590175d8f8095
Wambui Ndungu <wambuin18@gmail.com>
2024-10-25 21:40:56 +0300

    undo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,10 @@ import GetInvolved from "@/Sections/GetInvolved";
 import Hero from "@/Sections/Hero";
 import KnowAboutUs from "@/Sections/KnowAboutUs";
 import WhatWeDo from "@/Sections/WhatWeDo";
-import Image from "next/image";
 
 export default function Home() {
   return(
     <div className="">
-      <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
       <Hero />
       <KnowAboutUs />
       <WhatWeDo />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,7 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     screens: {
