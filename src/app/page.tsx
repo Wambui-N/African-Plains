2d1e5e5881356784297200f84c963a5e10a9987c
Wambui Ndungu <wambuin18@gmail.com>
2024-10-25 21:14:15 +0300

    c

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,14 @@ import Image from "next/image";
 export default function Home() {
   return(
     <div className="">
-      <Image src="/Icon.svg" alt="alt" width={500} height={500} />
+      <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
       <Hero />
       <KnowAboutUs />
       <WhatWeDo />
