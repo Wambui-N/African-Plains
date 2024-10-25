04460028ea31e4bcb8e39bf2a25621936fb16f1a
Wambui Ndungu <wambuin18@gmail.com>
2024-10-25 20:54:01 +0300

    experiment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@ import GetInvolved from "@/Sections/GetInvolved";
 import Hero from "@/Sections/Hero";
 import KnowAboutUs from "@/Sections/KnowAboutUs";
 import WhatWeDo from "@/Sections/WhatWeDo";
+import Image from "next/image";
 
 export default function Home() {
   return(
     <div className="">
+      <Image src="/Icon.svg" alt="alt" width={500} height={500} />
       <Hero />
       <KnowAboutUs />
       <WhatWeDo />
